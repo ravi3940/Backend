28cbc1dbc072620ec091fe5f493c918006962306
ravi3940 <ravikumar013013@gmail.com>
2024-09-03 11:20:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,54 +1,20 @@
-import express from  "express";
-import dotenv  from "dotenv";
-import cors from "cors"
-
-const app=express();
-  dotenv.config()
-
-const Port =process.env.PORT || 3000;
-
-
-const corsOptions = {
-    origin : "https:://localhost:5173"
-}
-// app.use(cors(corsOptions));
-app.get("/" ,(req,res) => {
-    res.send("Server is ready") ;
-     
+import express from  "express"
+import dotenv   from "dotenv";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 4000
+app.get("/",(req , res) => {
+    res.send("this is Home Page")
 })
 
-app.get("/api/jokes" ,(req,res)=> {
-    const jokes =[
-        {
-            "id": 1,
-            "title": "Why don't scientists trust atoms?",
-            "content": "Because they make up everything!"
-        },
-        {
-            "id": 2,
-            "title": "How do you organize a space party?",
-            "content": "You planet!"
-        },
-        {
-            "id": 3,
-            "title": "Why did the scarecrow win an award?",
-            "content": "Because he was outstanding in his field!"
-        },
-        {
-            "id": 4,
-            "title": "What do you call fake spaghetti?",
-            "content": "An impasta!"
-        },
-        {
-            "id": 5,
-            "title": "Why don’t skeletons fight each other?",
-            "content": "They don’t have the guts."
-        }
-    ]
-    res.send(jokes);
-    
-})
-app.listen(4000,(req , res)=>{
 
-    console.log(`server run at the port  ${Port}`)
+
+
+
+
+app.listen(port ,()=>{
+    console.log(`server run at this port ${port}`)
 })
+
+
+
